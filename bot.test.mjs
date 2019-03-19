2519fcc76d9b12532b51a1b57b7b079c6fe479e9
Walker <walker@praiseweb.com.br>
2019-03-19 09:32:13 -0300

    add onPost test

--- bot.test.mjs
+++ bot.test.mjs
@@ -6,19 +6,31 @@ import {createBot} from './bot'
 
 const feedDomain = 'http://foo.bar/'
 const feedPath = 'feeds'
-const feedFixture = path.join(path.dirname(''), 'fixtures/feed.xml')
+const feedFixture = path.join(path.dirname(''), 'fixtures/first-feed.xml')
+const feedLength = 20
 const feedURL = feedDomain + feedPath
 
 test.cb('call onPost with a new entry', t => {
     t.plan(1)
 
+    let itemsReceived = []
+
     nock(feedDomain)
         .get(feedPath)
         .replyWithFile(200, feedFixture)
 
-    const onPost = entry => {
-        t.deepEqual(entry, {})
+    const done = () => {
+        t.is(itemsReceived.length, feedLength)
+        nock.cleanAll()
         t.end()
+    }
+
+    const onPost = entry => {
+        itemsReceived.push(entry);
+
+        if (itemsReceived.length === feedLength) {
+            done();
+        }
     }
     createBot(feedURL, {onPost})
 })
