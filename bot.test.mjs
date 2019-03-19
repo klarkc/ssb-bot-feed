5cb79be80de835ad08fc034c555503506bdd32e5
Walker <walker@praiseweb.com.br>
2019-03-19 09:22:25 -0300

    sef feed mock and fixture

--- bot.test.mjs
+++ bot.test.mjs
@@ -1,11 +1,21 @@
+import path from 'path'
 import test from 'ava'
+import nock from 'nock'
 
 import {createBot} from './bot'
+
+const feedDomain = 'http://foo.bar/'
+const feedPath = 'feeds'
+const feedFixture = path.join(path.dirname(''), 'fixtures/feed.xml')
+const feedURL = feedDomain + feedPath
 
 test.cb('call onPost with a new entry', t => {
     t.plan(1)
 
-    const feedURL = 'http://github.com/nikezono.atom'
+    nock(feedDomain)
+        .get(feedPath)
+        .replyWithFile(200, feedFixture)
+
     const onPost = entry => {
         t.deepEqual(entry, {})
         t.end()
