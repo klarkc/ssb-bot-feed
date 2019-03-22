90fabeed7674a1c0dc456eff4ba0fb29e8e2421f
Walker <walker@praiseweb.com.br>
2019-03-22 00:22:41 -0300

    refactor bot to add create and destroy methods

--- index.mjs
+++ index.mjs
@@ -2,7 +2,11 @@ import ssbClient from 'ssb-client'
 import setup from './lib/setup'
 import bot from './lib/bot'
 
+const feedUrl = process.argv[3];
+if (feedUrl)
+    throw Error('feed url required')
+
 ssbClient(setup({
-    feedUrl: 'foo',
+    feedUrl,
     bot,
 }))
